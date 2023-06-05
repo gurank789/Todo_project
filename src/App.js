1a719234364eda1c817f75c49cc49bfeb83c5e6f
gurank789 <95094895+gurank789@users.noreply.github.com>
2023-06-06 00:02:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import KanbanBoard from './components/KanbanBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <KanbanBoard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
